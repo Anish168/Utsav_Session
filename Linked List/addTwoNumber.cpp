1feae0cc801515af50b35efad573f111a6248033
Anish168 <87292453+Anish168@users.noreply.github.com>
2023-01-21 23:32:07 +0530

    Add files via upload

---
+++ Linked List/addTwoNumber.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* anshead = new ListNode(0);
+        ListNode* curr = anshead;
+        ListNode* p = l1;
+        ListNode* q = l2;
+        int x,y = 0;
+        int carry = 0;
+        int sum =0;
+        while(p!=NULL || q!=NULL)
+        {
+            if(p!=NULL)
+            {
+                x = p->val;
+            }
+            else
+            {
+                x = 0;
+            }
+            if(q!=NULL)
+            {
+                y = q->val;
+            }
+            else
+            {
+                y = 0;
+            }
+            sum = x+y+carry;
+            carry = sum/10;
+            curr->next = new ListNode(sum%10);
+            curr = curr->next;
+            
+            if(p!=NULL)
+            {
+                p = p->next;
+            }
+            if(q!=NULL)
+            {
+                q=q->next;  
+            }
+
+        }
+        if(carry>0)
+        {
+            curr->next = new ListNode(carry);
+        }
+        return anshead->next;
+    }
+};
