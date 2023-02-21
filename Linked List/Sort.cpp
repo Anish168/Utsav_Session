409575161b5197caccee2b2e932d9d1969928ac6
Anish168 <87292453+Anish168@users.noreply.github.com>
2023-02-22 01:52:20 +0530

    Create Sort.cpp

---
+++ Linked List/Sort.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    ListNode* mid(ListNode* head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        // loop
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+    
+    ListNode* mergetwosortedll(ListNode *h1, ListNode *h2)
+    {
+        if (h1 == NULL)
+        {
+            return h2;
+        }
+        if (h2 == NULL)
+        {
+            return h1;
+        }
+
+        // recursive
+        ListNode *newhead;
+        if (h1->val < h2->val)
+        {
+            newhead = h1;
+            newhead->next = mergetwosortedll(h1->next, h2);
+        }
+        else
+        {
+            newhead = h2;
+            newhead->next = mergetwosortedll(h1, h2->next);
+        }
+        return newhead;
+        }
+    
+    ListNode* sortList(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+        ListNode *m = mid(head);
+        ListNode *l1h = head; // linked list1 ka head
+        ListNode *l2h = m->next;
+        m->next = NULL;
+
+        l1h = sortList(l1h);
+        l2h = sortList(l2h);
+        ListNode *fans = mergetwosortedll(l1h, l2h);
+        return fans;
+    }
+};
